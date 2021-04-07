d20e20a787b75bd3b1f4bd4548a25ccfcb90b0a2
MyeongHoonNam <codenmh0822@naver.com>
2021-04-07 13:44:25 +0900

    사용자이름정보 기능 구현

---
+++ js/gretting.js
@@ -0,0 +1,57 @@
+const nameContainer = document.querySelector(".js-name");
+
+function paintName(name){
+  nameContainer.innerHTML = "";
+
+  const span = document.createElement("span");
+
+  span.className = "name__text";
+  span.innerText = name;
+
+  nameContainer.appendChild(span);
+
+  return;
+}
+
+function handleSubmit(e){
+  e.preventDefault();
+
+  const name = e.target.querySelector("input").value;
+
+  localStorage.setItem("username", name);
+
+  paintName(`Hello ${name}`);
+
+  return;
+}
+
+function paintInput(){
+  const input = document.createElement("input");
+
+  input.placeholder = "Type your name here";
+  input.type = "text";
+  input.className = "name__input";
+
+  const form = document.createElement("form");
+
+  form.addEventListener("submit", handleSubmit);
+  form.appendChild(input);
+
+  nameContainer.appendChild(form);
+}
+
+function loadName(){
+  const name = localStorage.getItem("username");
+
+  if(name === null){
+    paintInput();
+  } else {
+    paintName(name);
+  }
+}
+
+function init(){
+  loadName();
+}
+
+init();
