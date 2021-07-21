935e769826873609b7f5cb7295bd551d2a8ff0b2
MyeongHoonNam <codenmh0822@naver.com>
2021-07-21 20:32:43 +0900

    addItem 함수 수정 : item cursor pointer 적용

--- CarrotFindGame/main.js
+++ CarrotFindGame/main.js
@@ -141,6 +141,7 @@ function addItem(className, count, imgPath) {
     item.setAttribute('class', className);
     item.setAttribute('src', imgPath);
     item.style.position = 'absolute';
+    item.style.cursor = 'pointer';
 
     const x = randomNumber(x1, x2);
     const y = randomNumber(y1, y2);
