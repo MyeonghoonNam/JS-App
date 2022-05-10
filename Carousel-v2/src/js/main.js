816fb4ec3458f7812d021f58e02cc780cd0597c6
MyeongHoonNam <codenmh0822@naver.com>
2022-05-10 14:54:44 +0900

    feat: 이미지 업로드 기능 구현

--- Carousel-v2/src/js/main.js
+++ Carousel-v2/src/js/main.js
@@ -1,1 +1,37 @@
-console.log("init");
+(() => {
+  const imageUploadInput = document.querySelector("#image-upload-input");
+  const carouselUl = document.querySelector(".carousel-list");
+
+  const createImageElement = (url) => {
+    const list = document.createElement("li");
+    const img = document.createElement("img");
+
+    list.classList.add("carousel-item");
+    list.appendChild(img);
+    img.src = url;
+
+    return list;
+  };
+
+  const imageUpload = (e) => {
+    const files = e.target.files;
+
+    if (files) {
+      const images = document.querySelectorAll(".carousel-item");
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+          const imageUrl = e.target.result;
+          carouselUl.insertBefore(createImageElement(imageUrl), images[0]);
+        };
+
+        reader.readAsDataURL(file);
+      }
+    }
+  };
+
+  imageUploadInput.addEventListener("change", imageUpload);
+})();
