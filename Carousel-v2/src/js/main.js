42be814503561ecd7062a1fbf72940a1298d19cc
MyeongHoonNam <codenmh0822@naver.com>
2022-05-11 16:28:06 +0900

    feat: 동적 이미지 업로드 기능 구현

--- Carousel-v2/src/js/main.js
+++ Carousel-v2/src/js/main.js
@@ -1,6 +1,47 @@
 (() => {
   const imageUploadInput = document.querySelector("#image-upload-input");
   const carouselUl = document.querySelector(".carousel-list");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  const setTranslate = () => {
+    const images = document.querySelectorAll(".carousel-item");
+    const size = images.length;
+    const distance = 100;
+    const space = 10;
+
+    if (size === 2) {
+      images[1].style.transform = `translateX(${distance + space}%)`;
+    } else if (size > 2) {
+      images.forEach((image, i) => {
+        if (i === 0) {
+          image.style.transform = `translateX(-${distance + space}%)`;
+        } else {
+          image.style.transform = `translateX(${
+            (i - 1) * distance + (i - 1) * space
+          }%)`;
+        }
+      });
+    }
+  };
+
+  const cloneElement = () => {
+    const images = document.querySelectorAll(".carousel-item");
+
+    if (images.length === 3) {
+      const cloneFirst = images[0].cloneNode(true);
+      const cloneLast = images[images.length - 1].cloneNode(true);
+
+      cloneFirst.classList.remove("now");
+      carouselUl.insertBefore(cloneLast, images[0]);
+      carouselUl.appendChild(cloneFirst);
+    } else {
+      const cloneFirst = images[1].cloneNode(true);
+      cloneFirst.classList.remove("now");
+      carouselUl.removeChild(images[images.length - 1]);
+      carouselUl.appendChild(cloneFirst);
+    }
+  };
 
   const createImageElement = (url) => {
     const list = document.createElement("li");
@@ -10,6 +51,13 @@
     list.appendChild(img);
     img.src = url;
 
+    const images = document.querySelectorAll(".carousel-item");
+    images.forEach((image) => {
+      image.classList.remove("now");
+    });
+
+    list.classList.add("now");
+
     return list;
   };
 
@@ -17,15 +65,35 @@
     const files = e.target.files;
 
     if (files) {
-      const images = document.querySelectorAll(".carousel-item");
-
       for (let i = 0; i < files.length; i++) {
         const file = files[i];
         const reader = new FileReader();
 
         reader.onload = (e) => {
+          const images = document.querySelectorAll(".carousel-item");
+          const size = images.length;
           const imageUrl = e.target.result;
-          carouselUl.insertBefore(createImageElement(imageUrl), images[0]);
+
+          if (size <= 2) {
+            carouselUl.insertBefore(createImageElement(imageUrl), images[0]);
+          } else {
+            carouselUl.insertBefore(createImageElement(imageUrl), images[1]);
+          }
+        };
+
+        reader.onloadend = () => {
+          const images = document.querySelectorAll(".carousel-item");
+
+          if (images.length > 2) {
+            cloneElement();
+          }
+
+          if (!prevBtn.style.display && !nextBtn.style.display) {
+            prevBtn.style.display = "block";
+            nextBtn.style.display = "block";
+          }
+
+          setTranslate();
         };
 
         reader.readAsDataURL(file);
